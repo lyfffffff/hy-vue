c43f6daf2026c836d7d8de1a6d261007931b798e
fe_luoyufeng <1443724970@qq.com>
2023-05-13 16:54:55 +0800

    feat: test
    test

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://152.136.185.210:4000/',
+        target: 'http://codercba.com:5000/',
         pathRewrite: {
           '^/api': ''
         },
